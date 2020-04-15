76ef53ca300ca05b6afdcd5a24aae23e2317f208
fanishe <fan.42anon@mail.ru>
2020-04-15 22:30:26 +0300

    test async

---
+++ testing/async_test.py
@@ -0,0 +1,29 @@
+import asyncio
+import random
+
+c = (
+    "\033[0m",
+    "\033[36m",
+    "\033[91m",
+    "\033[35m",
+)
+
+async def makerrandom(idx: int, threshold: int = 6) -> int:
+    print(c[idx + 1] + f"Initiated makerrandom({idx})")
+    i = random.randint(0, 10)
+    while i <= threshold:
+        print(c[idx + 1] + f"makerrandom({idx}) == too low; retrying.")
+        await asyncio.sleep(idx + 1)
+        i = random.randint(0, 10)
+    print(c[idx + 1] + f"---> Finished: makerdown({idx}) == {i}" + c[0])
+    return i
+
+async def main():
+    res = await asyncio.gather(*(makerrandom(i, 10 - i -1) for i in range(3)))
+    return res
+
+if __name__ == "__main__":
+    random.seed(444)
+    r1, r2, r3 = asyncio.run(main())
+    print()
+    print(f"r1 : {r1}, r2: {r2}, r3: {r3}")
