71d0058959ce5878c279d6c33ec5fc7320f24d3b
fanishe <fan.42anon@mail.ru>
2020-04-21 00:02:06 +0300

    I have some ideas

--- main2.py
+++ main2.py
@@ -3,6 +3,9 @@ from trees import My_Tree
 from threads import My_Thread, Logger
 import time
 import sys
+import asyncio
+import aiohttp
+from bs4 import BeautifulSoup
 
 
 var = sys.argv[1:]
@@ -19,21 +22,64 @@ var = sys.argv[1:]
 
 
 
-def main():
+# def main():
     # объект для записи логов и вывода в консоль
-    log = Logger()
-    log.write_log(f'START NEW SESSION')
-    
-    # принимает ссылку через терминал
-    # если ничего нет берет основной урл
-    if not var:
-        link = "https://avsw.ru"
-    else:
-        link = var[0]
-    
+    # pass
+
+log = Logger()
+log.write_log(f'START NEW SESSION')
+
+# принимает ссылку через терминал
+# если ничего нет берет основной урл
+if not var:
+    link = "https://avsw.ru"
+else:
+    link = var[0]
+# https://aiohttp.readthedocs.io/en/stable/client_quickstart.html
+
+async def get_html(link):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(link) as resp:
+            log.write_log(f'response - {resp.status}')
+            # print(await resp.text())
+
+async def parse_html(link):
+    response = await get_html(link)
+    social = ['vk', 'facebook', 'linkedin', 'twitter', 'callto', 'skype', 'tel', '#']
+    list_links = []
+    soup = BeautifulSoup(response, 'html.parser')
+    all_links = soup.find_all('a')
+
+    for link in all_links:
+        # забирает href
+        l = link.get('href')
+
+        if l:
+            # убираю аргументы из ссылок
+            if '?' in l:
+                l = l[:l.find('?')]
+
+            # if hostname in l:
+            #     # отделить домен
+            #     l = l.split(hostname)
+            #     l = l[-1]
+
+            for s in  social:
+                # удалить ссылки на соцсеточки
+                if s in l :
+                    l = 'zero'
+
+            # сохраняю в список
+            if l not in  list_links and l != 'zero' and len(l) > 1:
+                 list_links.append(l)
+                 print(l)
+    return list_links
+
+
 
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(parse_html(link))
+
     
